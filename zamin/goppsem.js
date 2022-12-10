57d2caad2e506da5bdc56a001804332f20f37823
leeza <118030388+jasmin0325@users.noreply.github.com>
2022-12-10 09:55:52 +0900

    곱셈

--- zamin/goppsem.js
+++ zamin/goppsem.js
@@ -1,7 +1,7 @@
-var 곱 = 1
+function 구구콘(x) {
+    for (var i = 1; i < 10; i++) {
+        console.log(x * i)
+    }
+}
+구구콘(7)
 
- function 구구(x) {
-     for (var i = 1; i < 10; i++) {
-         console.log(x * i)
-     }
- } 구구(7)
