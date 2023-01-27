dda07403e25b6001cf4228dce725c0b0d5b0a154
ijjamminn <lee.ja.min325@gmail.com>
2023-01-27 23:40:16 +0900

    1.27 숙제

--- zamin/python_hw/Day3_hw.py
+++ zamin/python_hw/Day3_hw.py
@@ -1,6 +1,73 @@
-a=0
-while a<3:
-     print("숙제다")
-     a+=1
-for a in range(3):
-    print("숙제다")
+#1번문제
+# a=0
+# while a<3:
+#      print("숙제다")
+#      a+=1
+# for a in range(3):
+#     print("숙제다")
+#2번문제
+# a=0
+# 합=0
+# while a<11: 
+#      if a%2!=0:
+#          합+=a
+#      a+=1
+# print(합)
+
+# 합함=0
+# for i in range(10):
+#      if i%2!=0:
+#           합함+=i
+# print(합함)
+#3번문제 1~20 중 5의 배수가 아닌 수들의 곱
+# a=0
+# 곱=1
+# while a<21:
+#      if a%5!=0:
+#           곱*=a
+#      a+=1
+# print(곱)
+# 곱함=1
+# for a in range(21):
+#      if a%5!=0:
+#           곱함*=a
+# print(곱함)
+#4번문제 1~100중 2,3,5의 공배수
+# a=0
+# while a<101:
+#      if a%2==0:
+#           if a%3==0:
+#                if a%5==0:
+#                     print(a)
+#      a+=1
+# for i in range(101):
+#      if i%2==0:
+#           if i%3==0:
+#                if i%5==0:
+#                     print(i)
+#5번문제 1~100중 짝수합-홀수합
+# a=1
+# 짝합=0
+# 합=0
+# while a<101:
+#      if a%2==0:
+#          짝합+=a
+#      if a%3==0:
+#          합+=a
+#      a+=1
+# print(짝합-합)
+# 짝합=0
+# 홀합=0
+# for i in range(1,101):
+#      if i%2==0:
+#           짝합+=i
+#      if i%3==0:
+#           홀합+=i
+# print(짝합-홀합)
+
+
+
+
+
+
+
