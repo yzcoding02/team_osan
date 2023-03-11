0d23428bb0b8a026c5b79ee6abe0f6f845136d69
ijjamminn <lee.ja.min325@gmail.com>
2023-03-11 11:24:40 +0900

    day8

---
+++ zamin/python_hw/Day8_hw.py
@@ -0,0 +1,7 @@
+# import matplotlib.pyplot as plt
+# fruits= ["apple","banana","cherry","orange"]
+# counts=[4,7,6,9]
+# figure,axes=plt.subplots(1,2)
+# axes[0].plot([5,0],[0,5])
+# axes[1].plot([0,5],[0,5])
+# plt.show()
