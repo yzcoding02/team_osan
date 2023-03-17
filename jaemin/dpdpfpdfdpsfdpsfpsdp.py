372011026a4e3db0ec41114280963d297681ce11
jaeminstan <hwasan93@gmail.com>
2023-03-18 08:38:05 +0900

    day7nothw.py

---
+++ jaemin/dpdpfpdfdpsfdpsfpsdp.py
@@ -0,0 +1,28 @@
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot([1,0],[0,-1],[-1,0],[0,1])
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("uzbekistan")
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot([-1,0],[0,1],[-1,0],[0,-1],[1,0],[0,-1],[1,0],[0,1])
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("uzbekistan")
+# plt.show()
+
+
+
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot([0,5],[2,0],[-1,5],[0,6],[5,5],[0,6])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("uzbekistan")
+plt.show()
