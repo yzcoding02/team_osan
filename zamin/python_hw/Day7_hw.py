89672a34ad28c8cfccaeede0c62662b62dc1d328
ijjamminn <lee.ja.min325@gmail.com>
2023-03-18 10:05:00 +0900

    ep.7

--- zamin/python_hw/Day7_hw.py
+++ zamin/python_hw/Day7_hw.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.figure()
-plt.plot([-1,0],[0,1],[1,0],[0,-1],[-1,0],[0,-1],[1,0],[0,1])
-plt.xlabel("x")
-plt.ylabel("y")
-plt.show()
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot([-1,0],[0,1],[1,0],[0,-1],[-1,0],[0,-1],[1,0],[0,1])
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.show()
