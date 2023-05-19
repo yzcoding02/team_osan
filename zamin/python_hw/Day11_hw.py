c2e39173ae31e791ef5c47e133074848b0002dcb
ijjamminn <lee.ja.min325@gmail.com>
2023-05-20 08:54:00 +0900

    11홈

---
+++ zamin/python_hw/Day11_hw.py
@@ -0,0 +1,20 @@
+for i in range(1,6):
+    class math:
+        def 부피(slef,a,b):
+            부=3.14*a*a*b
+            return 부
+        def 겉넓이이(self,c,e):
+            값=2*3.14*c*e+3.14*c*c
+            return 값
+    반지름=input("반지름 입력:")
+    높=input("높이 입력:")
+    중등수학=math()
+    print(f"반지름이 {반지름}이며,높이가 {높}일때 원기둥의 겉넓이는{중등수학.겉넓이이(int(반지름),int(높))}이고 원기둥의 부피는 {중등수학.부피(int(반지름),int(높))}입니다")
+    class suhak:
+        def 넓이(self,a,b):
+            값은=a*b/2
+            return 값은
+    밑변은=input("밑변입력:")
+    높이는=input("높이입력:")
+    수확=suhak()
+    print(f"밑변이{밑변은}이고 높이가{높이는}일때에 삼각형 넓이는{suhak.넓이(int(밑변은),int(높이는))}임")
