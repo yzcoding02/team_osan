d9693648564c768b0cc379162f9d57b2fd169650
pentaggram10 <kyg44112@gmail.com>
2023-09-02 10:16:38 +0900

    456

--- yonggyu/python_HW/day19-hw.py
+++ yonggyu/python_HW/day19-hw.py
@@ -1,6 +1,6 @@
 import turtle as t
 t.speed(0)
-def allClear(x,y):
+def allClear():
     t.clear()
 def jump(x,y):
     t.penup()
@@ -12,13 +12,29 @@ def mcircle():
     t.circle(80)
 def bcircle():
     t.circle(120)
-
-t.onkey(scircle,"s")
-t.onkey(mcircle,"m")
-t.onkey(bcircle,"b")
+def red():
+    t.color("red")
+def blue():
+    t.color("blue")
+def green():
+    t.color("green")
+def white():
+    t.color("white")
+def thick():
+    t.pensize(5)
+def thin():
+    t.pensize(1)
+t.onkey(scircle,"1")
+t.onkey(mcircle,"2")
+t.onkey(bcircle,"3")
 t.onscreenclick(jump)
-t.onscreenclick(allClear,3)
-'''3 is rmb '''
-
+t.onkey(allClear,"c")
+t.onkey(red,"r")
+t.onkey(blue,"b")
+t.onkey(green,"g")
+t.onkey(white,"w")
+t.onkey(thick,"t")
+t.onkey(thin,"h")
+t.ondrag(t.goto)
 t.listen()
 t.mainloop()
